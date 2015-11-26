47ab4bd1e93c534d3e6293771a5b23465e266e4c
Ken <kenjyco@gmail.com>
2015-11-26 14:36:36 -0600

    Add a url to 'setup.py' and fix a typo

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from setuptools import setup
 setup(
     name='redis_helper',
     version='0.1.0',
-    description='Helper functions to store/retreive redis objects',
+    description='Helper functions to store/retrieve redis objects',
     author='Ken',
     author_email='kenjyco@gmail.com',
+    url='https://github.com/kenjyco/redis_helper',
     packages=['redis_helper'],
     install_requires=[
         'redis>=2.10,<3.0',
