b25947b145ef4d7f0e65c37bbb1e9a5c7f19563a
Ken <kenjyco@gmail.com>
2017-01-28 11:27:42 -0600

    Update 'RedThing.get_by_position' to pass kwargs to '.get'

--- redis_helper/redthing.py
+++ redis_helper/redthing.py
@@ -219,19 +219,15 @@ class RedThing(object):
             )
         return data
 
-    def get_by_position(self, pos):
+    def get_by_position(self, pos, **kwargs):
+        """Wrapper to self.get"""
         data = {}
+        if not kwargs:
+            kwargs['include_meta'] = True
         x = rh.REDIS.zrange(self._ts_zset_key, pos, pos, withscores=True)
         if x:
             hash_id, ts = x[0]
-            data = self.get(hash_id)
-            data['_id'] = rh.decode(hash_id)
-            data['_ts_raw'] = ts
-            data['_ts_admin'] = rh.utc_float_to_pretty(
-                ts,
-                fmt=rh.ADMIN_DATE_FMT,
-                timezone=rh.ADMIN_TIMEZONE
-            )
+            data = self.get(hash_id, **kwargs)
         return data
 
     @property
