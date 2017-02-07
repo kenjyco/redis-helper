fc75abe0cd059269d019ab17a2249a9dd1e9fe66
Ken <kenjyco@gmail.com>
2017-02-07 08:03:31 -0600

    Import input_helper as ih before starting embedded shell and notify user

--- redis_helper/scripts/shell.py
+++ redis_helper/scripts/shell.py
@@ -5,8 +5,10 @@ import click
 def main():
     from IPython import embed
     import redis_helper as rh
+    import input_helper as ih
     model = rh.Collection.select_model()
     print('\nmodel={}\n'.format(repr(model)))
+    print('import redis_helper as rh\nimport input_helper as ih\n\n')
     embed()
 
 
