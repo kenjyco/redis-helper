914ebf091254dd3c5a8e19c60f90d0354881b722
Ken <kenjyco@gmail.com>
2017-02-19 09:58:47 -0600

    Update dev-setup.bash to use correct path to settings.ini file

--- dev-setup.bash
+++ dev-setup.bash
@@ -3,11 +3,11 @@
 # Get the directory where this script lives
 DIR="$(cd "$(dirname "$0")" && pwd)"
 
-[[ ! -d "venv" ]] && python3 -m venv venv && venv/bin/pip3 install wheel
-venv/bin/pip3 install pip --upgrade
+cd "$DIR"
+[[ ! -d "venv" ]] && python3 -m venv venv && venv/bin/pip3 install pip wheel --upgrade
 venv/bin/pip3 install -r requirements.txt --upgrade
 
 if [[ ! -f "$HOME/.config/redis-helper/settings.ini" ]]; then
     mkdir -pv "$HOME/.config/redis-helper"
-    cp -av settings.ini "$HOME/.config/redis-helper"
+    cp -av redis_helper/settings.ini "$HOME/.config/redis-helper"
 fi
