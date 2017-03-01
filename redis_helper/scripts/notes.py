ff10849e8528e95f5961c9a494fbc7b2655a71f7
Ken <kenjyco@gmail.com>
2017-03-01 10:08:12 -0600

    Use 'ih.user_input_fancy' func in scripts/notes.py

--- redis_helper/scripts/notes.py
+++ redis_helper/scripts/notes.py
@@ -1,7 +1,6 @@
 import click
 import input_helper as ih
 import redis_helper as rh
-from input_helper import matcher
 
 
 class Notes(rh.Collection):
@@ -10,30 +9,27 @@ class Notes(rh.Collection):
         'backtick_list',
         'capitalized_phrase_list',
         'doublequoted_list',
-        'line_comment',
         'mention_list',
-        'non_comment',
         'paren_group_list',
         'singlequoted_list',
-        'tag_list'
+        'tag_list',
+        'url_list',
     ]
-    m = matcher.SpecialTextMultiMatcher()
 
     def __init__(self, *args, **kwargs):
         kwargs['json_fields'] = ','.join(self.json_field_list)
         super().__init__(*args, **kwargs)
 
-    def parse_and_add(self, text, topic):
-        """Parse text using 'self.m' and add using 'self.add'
+    def add_parsed(self, parsed_text, topic):
+        """Modify parsed_text and add using 'self.add'
 
         For now, the first #tag and @mention will also be separate attributes
         """
-        special_text = self.m(text)
-        if 'tag_list' in special_text:
-            special_text['tag'] = special_text['tag_list'][0]
-        if 'mention_list' in special_text:
-            special_text['mention'] = special_text['mention_list'][0]
-        self.add(text=text, topic=topic, **special_text)
+        if 'tag_list' in parsed_text:
+            parsed_text['tag'] = parsed_text['tag_list'][0]
+        if 'mention_list' in parsed_text:
+            parsed_text['mention'] = parsed_text['mention_list'][0]
+        self.add(topic=topic, **parsed_text)
 
 
 notes = Notes(
@@ -64,9 +60,9 @@ def main(ch, shell, topic):
 
     print('\nPress <Enter> twice to stop prompting.\n')
     while True:
-        text = ih.user_input(topic, ch)
-        if text:
-            notes.parse_and_add(text, topic)
+        parsed_text = ih.user_input_fancy(topic, ch)
+        if parsed_text['text']:
+            notes.add_parsed(parsed_text, topic)
         else:
             break
 
