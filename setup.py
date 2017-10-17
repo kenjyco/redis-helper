9b7cc96c546a113079a0f912d6f17a357ce73fe0
Ken <kenjyco@gmail.com>
2017-10-17 08:06:37 -0500

    Bump to v0.3.20

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ with open('README.rst', 'r') as fp:
 
 setup(
     name='redis-helper',
-    version='0.3.19',
+    version='0.3.20',
     description='Easily store, index, and modify Python dicts in Redis (with flexible searching)',
     long_description=long_description,
     author='Ken',
     author_email='kenjyco@gmail.com',
     license='MIT',
     url='https://github.com/kenjyco/redis-helper',
-    download_url='https://github.com/kenjyco/redis-helper/tarball/v0.3.19',
+    download_url='https://github.com/kenjyco/redis-helper/tarball/v0.3.20',
     packages=find_packages(),
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
