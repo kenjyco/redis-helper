e1a431be9e6765be84456b1676be0696be6404e6
Ken <kenjyco@gmail.com>
2017-12-07 10:24:08 -0600

    Update test_settings.py to use settings_helper.APP_ENV

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,9 +1,10 @@
 import redis_helper as rh
+import settings_helper as sh
 
 
 class TestEnv:
     def test_app_env_is_test(self):
-        assert rh.APP_ENV == 'test'
+        assert sh.APP_ENV == 'test'
 
     def test_redis_url_ends_with_9(self):
         redis_url = rh.get_setting('redis_url')
