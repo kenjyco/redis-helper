347a750154e1b00305a837a79cbcfe9c9a22ce89
Ken <kenjyco@gmail.com>
2018-10-24 21:35:12 -0500

    Update rh-shell script to not die ugly when no Collections exist

--- redis_helper/scripts/shell.py
+++ redis_helper/scripts/shell.py
@@ -8,9 +8,14 @@ def main():
     from IPython import embed
     import redis_helper as rh
     import input_helper as ih
-    model, *other_models = rh.Collection.select_models()
+    try:
+        model, *other_models = rh.Collection.select_models()
+    except ValueError:
+        model = None
+        other_models = None
     print('\nimport redis_helper as rh\nimport input_helper as ih\n')
-    print('model=rh.{}\n'.format(repr(model)))
+    if model:
+        print('model=rh.{}\n'.format(repr(model)))
     if other_models:
         print('# Check "other_models" for the {} other model(s) you selected\n\n'.format(
             len(other_models)
