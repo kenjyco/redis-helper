6b4ca819d5ad64e4598e39a93aa6ab5b62f4dcd2
Ken <kenjyco@gmail.com>
2019-04-14 21:44:04 -0500

    Update dev-setup.bash to install redis-helper to venv

--- dev-setup.bash
+++ dev-setup.bash
@@ -6,6 +6,7 @@ DIR="$(cd "$(dirname "$0")" && pwd)"
 cd "$DIR"
 [[ ! -d "venv" ]] && python3 -m venv venv && venv/bin/pip3 install pip wheel --upgrade
 venv/bin/pip3 install -r requirements.txt --upgrade
+venv/bin/python3 setup.py develop
 
 if [[ ! -f "$HOME/.config/redis-helper/settings.ini" ]]; then
     mkdir -pv "$HOME/.config/redis-helper"
