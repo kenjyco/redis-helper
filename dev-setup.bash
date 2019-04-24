80eb0c6623f42ac5671d52b90aa3f474eedeabe8
Ken <kenjyco@gmail.com>
2019-04-24 05:42:27 -0500

    Update dev-setup.bash to use eager upgrade strategy

--- dev-setup.bash
+++ dev-setup.bash
@@ -5,7 +5,12 @@ DIR="$(cd "$(dirname "$0")" && pwd)"
 
 cd "$DIR"
 [[ ! -d "venv" ]] && python3 -m venv venv && venv/bin/pip3 install pip wheel --upgrade
-venv/bin/pip3 install -r requirements.txt --upgrade
+pip_args=(--upgrade)
+pip_version=$(venv/bin/pip3 --version | egrep -o 'pip (\d+)' | cut -c 5-)
+[[ -z "$pip_version" ]] && pip_version=$(venv/bin/pip3 --version | perl -pe 's/^pip\s+(\d+).*/$1/')
+[[ -z "$pip_version" ]] && pip_version=0
+[[ $pip_version -gt 9 ]] && pip_args=(--upgrade --upgrade-strategy eager)
+venv/bin/pip3 install -r requirements.txt ${pip_args[@]}
 venv/bin/python3 setup.py develop
 
 if [[ ! -f "$HOME/.config/redis-helper/settings.ini" ]]; then
