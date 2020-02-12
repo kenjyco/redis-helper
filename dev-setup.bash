038bd072f0e4f833b3f8fd81d8825e7bc1336c3c
Ken <kenjyco@gmail.com>
2020-02-12 09:52:40 -0600

    Update dev-setup to be similar to latest ~/bin/python/venv-setup
    See:
    https://github.com/kenjyco/dotfiles/blob/19b4f91462f4e2966709e15b627a0e720a709edb/bin/python/venv-setup

--- dev-setup.bash
+++ dev-setup.bash
@@ -4,14 +4,23 @@
 DIR="$(cd "$(dirname "$0")" && pwd)"
 
 cd "$DIR"
-[[ ! -d "venv" ]] && python3 -m venv venv && venv/bin/pip3 install pip wheel --upgrade
-pip_args=(--upgrade)
-pip_version=$(venv/bin/pip3 --version | egrep -o 'pip (\d+)' | cut -c 5-)
-[[ -z "$pip_version" ]] && pip_version=$(venv/bin/pip3 --version | perl -pe 's/^pip\s+(\d+).*/$1/')
-[[ -z "$pip_version" ]] && pip_version=0
-[[ $pip_version -gt 9 ]] && pip_args=(--upgrade --upgrade-strategy eager)
-venv/bin/pip3 install -r requirements.txt ${pip_args[@]}
-venv/bin/python3 setup.py develop
+pip_args=(--no-warn-script-location --upgrade --upgrade-strategy eager)
+PYTHON="python3"
+PIP="venv/bin/pip3"
+if [[ $(uname) =~ "MINGW" ]]; then
+    PYTHON="python"
+    PIP="venv/Scripts/pip"
+fi
+[[ ! -d venv ]] && $PYTHON -m venv venv
+PYTHON=$(dirname $PIP)/python
+$PYTHON -m pip install --upgrade pip wheel
+$PIP install -r requirements.txt ${pip_args[@]}
+if [[ ! $(uname) =~ "MINGW" ]]; then
+    $PIP install ipython pytest pdbpp ${pip_args[@]}
+else
+    $PIP install ipython pytest ${pip_args[@]}
+fi
+$PYTHON setup.py develop
 
 if [[ ! -f "$HOME/.config/redis-helper/settings.ini" ]]; then
     mkdir -pv "$HOME/.config/redis-helper"
